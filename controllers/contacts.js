50f84545307cce07008368f3e7e1fe55cfc6b1d6
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-06-23 15:10:31 +0200

    add filter by favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,14 +3,20 @@ const { CtrlWrapper, HttpError } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite = null } = req.query;
 
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, null, { skip, limit }).populate(
-    "owner",
-    "email"
-  );
+  const query = { owner };
+
+  if (favorite) {
+    query.favorite = favorite;
+  }
+
+  const result = await Contact.find(query, null, {
+    skip,
+    limit,
+  }).populate("owner", "email");
   if (!result) {
     throw HttpError(404, "not found");
   }
