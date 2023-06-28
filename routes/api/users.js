c28da15c3f5f67728f45e60538d27dc71ae642ba
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-06-28 11:51:52 +0200

    add update endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,7 +15,7 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch(
-  "/users",
+  "/update",
   authenticate,
   validateBody(schemas.subscriptionSchema),
   ctrl.updateSubscription
